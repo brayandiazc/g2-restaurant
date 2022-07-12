f899be1b1bcc0797e94eacb9b444eaabe590f436
Brayan Diaz C <brayandiazc@gmail.com>
2022-07-12 11:19:43 -0500

    Permitir editar usuario y su rol

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[ email password password_confirmation remember_me
+                      name bio category birthday role
+    ]
+
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[email password remember_me]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path(resource)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
-  get 'pages/terms'
+  devise_for :users, controllers: { registrations: "registrations" },
+                     path: "",
+                     path_names: { sign_in: "login",
+                                   sign_out: "logout",
+                                   sign_up: "register" }
+  get "pages/home"
+  get "pages/terms"
   resources :categories
   root "pages#home"
 end
